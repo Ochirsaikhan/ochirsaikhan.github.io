18325f5ec2d8b8d09a67ce1dc8cf5b1c90d86461
Ochirsaikhan <davaajambal01@allegheny.edu>
2022-11-18 16:12:42 -0500

    Add smooth scroll to projects

--- site/assets/js/script.js
+++ site/assets/js/script.js
@@ -1,36 +1,45 @@
-$(document).ready(function() {
+$(document).ready(function () {
   "use strict";
   // Scroll to top
-  $("a[href='#top']").click(function() {
+  $("a[href='#top']").click(function () {
     $("html, body").animate({ scrollTop: 0 }, "slow");
     return false;
   });
 
+  $("a[href='/#project']").click(function () {
+    $("html, body").animate({ scrollTop: $("#project").offset().top }, "slow");
+    return false;
+  });
+
   // Smooth scroll
-  $('a.scroll-to').on('click', function (event) {
+  $("a.scroll-to").on("click", function (event) {
     var $anchor = $(this);
-    $('html, body').stop().animate({
-        scrollTop: ($($anchor.attr('href')).offset().top - 50)
-    }, 700);
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $($anchor.attr("href")).offset().top - 50,
+        },
+        700
+      );
     event.preventDefault();
   });
 
-  $('.site-testimonial-item').on('mouseenter', function(){
-    $('.site-testimonial-item').addClass('inactive');
-    $(this).removeClass('inactive').addClass('active');
+  $(".site-testimonial-item").on("mouseenter", function () {
+    $(".site-testimonial-item").addClass("inactive");
+    $(this).removeClass("inactive").addClass("active");
   });
-  $('.site-testimonial-item').on('mouseleave', function(){
-    $('.site-testimonial-item').removeClass('inactive');
-    $('.site-testimonial-item').removeClass('active');
+  $(".site-testimonial-item").on("mouseleave", function () {
+    $(".site-testimonial-item").removeClass("inactive");
+    $(".site-testimonial-item").removeClass("active");
   });
-
 });
 
-$(window).on('scroll', function () {
+$(window).on("scroll", function () {
   var windscroll = $(window).scrollTop();
   if (windscroll >= 100) {
-    $('.site-navigation').addClass('nav-bg');
+    $(".site-navigation").addClass("nav-bg");
   } else {
-    $('.site-navigation').removeClass('nav-bg');
+    $(".site-navigation").removeClass("nav-bg");
   }
 });
